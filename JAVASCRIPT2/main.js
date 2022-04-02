eb73316b77ee1af346e6eec17a11f7a00cb6a380
Emmanuel02212 <100346837+Emmanuel02212@users.noreply.github.com>
2022-04-02 17:38:20 +0100

    Add files via upload

---
+++ JAVASCRIPT2/main.js
@@ -0,0 +1,28 @@
+let s = 60
+let m = 60
+let h = 24
+
+let contain = document.querySelector(".js-contain");
+
+function count(){
+    if(h == 0 && m == 0 && s == 0){
+        alert("nice one");
+        clearInterval(x)
+        
+    } else if(h!== 0 && m !== 0 && s== 0){
+        m --
+        s = 59
+
+    }else if(h!==0 && m == 0 && s==0){
+        h--
+        m = 59
+        s = 59
+    }else if (h == 0 && m !== 0 && s == 0){
+        m--
+        s = 59
+    }else{
+        s--
+    }
+    contain.innerText = `${h}hr ${m}m ${s}s`
+}
+let x = setInterval(count, 1000)
